6837f7210036cfebb9eff2d6b2227c1734b823d8
nicoc <norfuen@hotmail.com>
2017-09-07 13:21:28 -0300

    Finalizado tp3

--- tp3/start.sh
+++
@@ -1,3 +0,0 @@
-# Antes crear volumen -> docker volume create dss_bd_tp3
-docker run -d -p 5432:5432 -v dss_bd_tp3:/var/lib/postgresql/data --name postgres_container postgres
-echo "Run postgres_container"
