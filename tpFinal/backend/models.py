c8a0db9cdc9d0ddd3e8baa74ea7740895190ed6e
Damián Mansilla <fdmansilla333@gmail.com>
2017-11-11 22:19:18 -0300

    Funcionando backends

--- tpFinal/backend/models.py
+++ tpFinal/backend/models.py
@@ -28,12 +28,31 @@ MODELOS = [
         "path":'modelos/Randomforest.pickle',
         "instancia":None
     },
-    #{
-    #    "nombre": 'RedNeuronal',
-    #    "path":'modelos/clasificadorNET-Damian1-score(0.9).pickle',
-    #    "instancia":None
-    #}
+    {
+        "nombre": 'CNN',
+        "path":'modelos/clasificadorNET-Damian1-score(0.9).pickle',
+        "instancia":None
+    }
 ]
+
+class AdjustVariable(object):
+
+    """
+    Used to decreases linearly the learning rate with the number of epochs,
+    while we the momentum increase.
+    """
+    def __init__(self, name, start=0.01, stop=0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+
+        epoch = train_history[-1]['epoch']
+        new_value = np.float32(self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
 
 class Predictor:
 
@@ -49,13 +68,11 @@ class Predictor:
         return True
 
     def predecir(self, np_array):
-
         if not self._modelos_cargados:
             return {
                 "estado": -1,
                 "result":"Error: Los modelos no fueron cargados" 
             }
-             
             
         if np_array.shape != (28,28):
             return {"estado": -2,
@@ -65,9 +82,12 @@ class Predictor:
         a = np_array.reshape( 1, 784)
         result = []
         for modelo in MODELOS:
+            if modelo['nombre'] == 'CNN':
+                a = a.reshape(-1, 1, 28,28)
+            _prediccion = modelo.get('instancia').predict(a)[0]
             result.append({
                 "nombre_modelo": modelo.get('nombre'),
-                "label": CLASES[modelo.get('instancia').predict(a)[0]]
+                "label": CLASES[_prediccion]
             })
 
         return {
