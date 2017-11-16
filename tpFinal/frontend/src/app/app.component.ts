f6efa60be3b507220b972dd784b4b60af4894e11
Damián Mansilla <fdmansilla333@gmail.com>
2017-11-15 22:36:04 -0300

    Agregando frontend con previsualizacion de imagenes

--- tpFinal/frontend/src/app/app.component.ts
+++ tpFinal/frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Respuesta } from './respuesta';
 import { RequestOptions, Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FileDropModule, UploadFile, UploadEvent } from 'ngx-file-drop/lib/ngx-drop';
 import { AppService } from './app.service';
+import { element } from 'protractor';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -15,23 +17,49 @@ export class AppComponent {
   title = 'app';
   public error: String = '';
   public respuesta: Respuesta;
+  @Input() public url: any;
 
   constructor(public servicio: AppService, private http: Http) { }
 
   public files: UploadFile[] = [];
-  public dropped(event: UploadEvent) {
+  public cargando: Boolean = false;
+  public nombreImagen: String;
+
+
+  public dropped(event: any) {
+    
+   
     this.files = event.files;
     for (let file of event.files) {
       file.fileEntry.file(info => {
         console.log(info);
+        this.crearImagen(info);
+
       });
     }
+    console.log('Fin....droped');
   }
+
+  public crearImagen(data: Blob) {
+    console.log('creandoimagen');
+    let reader = new FileReader();
+    reader.addEventListener('load', () => {
+      this.url = reader.result;
+      this.cargando = true;
+      console.log(this.url);
+
+    }, false);
+    if (data) {
+      console.log('reader');
+      reader.readAsDataURL(data);
+    }
+  }
+
+
   public fileOver(event) {
-    console.log(event);
   }
   public fileLeave(event) {
-    console.log(event);
+
   }
   public enviarArchivos() {
     this.error = '';
@@ -56,8 +84,8 @@ export class AppComponent {
         let file: File = fileList[0];
         let formData: FormData = new FormData();
         formData.append('inputImagen', file, file.name);
-  
-  
+
+
         this.http.post('/prediccion', formData)
           .subscribe(
           data => console.log('success'),
